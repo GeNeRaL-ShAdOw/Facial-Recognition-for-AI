4852b56e10f40382dfbfdd4fd195a2d2240d0370
GeNeRaL-ShAdOw <51219966+GeNeRaL-ShAdOw@users.noreply.github.com>
2022-04-07 11:06:37 +0530

    Update Facial Recognition.py

--- Facial Recognition.py
+++ Facial Recognition.py
@@ -6,7 +6,7 @@ subjects = ["", "Ramiz Raja", "Elvis Presley"]
 
 def detect_face(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
+    face_cascade = cv2.CascadeClassifier('C:/Users/chinm/Downloads/AI Project Face Recog/opencv-files/lbpcascade_frontalface.xml')
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
     if (len(faces) == 0):
         return None, None
@@ -39,11 +39,13 @@ def prepare_training_data(data_folder_path):
     return faces, labels
 
 print("Preparing data...")
-faces, labels = prepare_training_data("training-data")
+faces, labels = prepare_training_data("C:/Users/chinm/Downloads/AI Project Face Recog/training-data")
 print("Data prepared")
 print("Total faces: ", len(faces))
 print("Total labels: ", len(labels)) 
+
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
 face_recognizer.train(faces, np.array(labels))
 def draw_rectangle(img, rect):
     (x, y, w, h) = rect
@@ -51,17 +53,18 @@ def draw_rectangle(img, rect):
 def draw_text(img, text, x, y):
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 def predict(test_img):
+    if test_img is None:
+        return
     img = test_img.copy()
     face, rect = detect_face(img)
     label, confidence = face_recognizer.predict(face)
     label_text = subjects[label]
     draw_rectangle(img, rect)
     draw_text(img, label_text, rect[0], rect[1]-5)
-    
     return img
 print("Predicting images...")
-test_img1 = cv2.imread("test-data/test1.jpg")
-test_img2 = cv2.imread("test-data/test2.jpg")
+test_img1 = cv2.imread("C:/Users/chinm/Downloads/AI Project Face Recog/test-data/test1.jpg")
+test_img2 = cv2.imread("C:/Users/chinm/Downloads/AI Project Face Recog/test-data/test2.jpg")
 predicted_img1 = predict(test_img1)
 predicted_img2 = predict(test_img2)
 print("Prediction complete")
